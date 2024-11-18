1c7e7fbde7397b9e7b37278a5f3768431d22d860
Mykola <shyshkinmykola@gmail.com>
2024-11-18 13:23:45 +0100

    adding solution with tasks

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.WriteLine("Калькулятор");
+
+Console.WriteLine("Введіть перше ціле число");
+int firstNum = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введіть друге ціле число");
+int secondNum = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введіть одну з цих математичних дій: 1.+ 2.- 3.* 4./");
+int operation = Convert.ToInt32(Console.ReadLine());
+
+switch (operation)
+{
+    case 1:
+        int result1 = firstNum + secondNum;
+        Console.WriteLine($"В суммі {firstNum} і {secondNum} дають {result1}");
+        break;
+    case 2:
+        int result2 = firstNum - secondNum;
+        Console.WriteLine($"Різниця {secondNum} від {firstNum} буде {result2}");
+        break;
+    case 3:
+        int result3 = firstNum * secondNum;
+        Console.WriteLine($"Добутком {firstNum} і {secondNum} є {result3}");
+        break;
+    case 4:
+        int result4 = firstNum / secondNum;
+        Console.WriteLine($"При діленні {firstNum} на {secondNum} результат {result4}");
+        break;
+    default:
+        Console.WriteLine("В функціоналі є лише 4 дії");
+        break;
+}
---
+++ Calculator/Calendar/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Введіть номер тижня і натисніть Enter, щоб отримати його назву.");
+int dayNum = Convert.ToInt32(Console.ReadLine());
+switch (dayNum){
+    case 1:
+        Console.WriteLine("Понеділок");
+        break;
+    case 2:
+        Console.WriteLine("Вівторок");
+        break;
+    case 3:
+        Console.WriteLine("Середа");
+        break;
+    case 4:
+        Console.WriteLine("Четвер");
+        break;
+    case 5:
+        Console.WriteLine("П'ятниця");
+        break;
+    case 6:
+        Console.WriteLine("Субота");
+        break;
+    case 7:
+        Console.WriteLine("Неділя");
+        break;
+    default:
+        Console.WriteLine("Потрібні числа від 1 до 7");
+        break;
+}
