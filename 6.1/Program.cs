0b0fa5fedbfa65ba9aab6829106ea1617393805d
Mykola <shyshkinmykola@gmail.com>
2024-12-05 15:19:38 +0100

    Added task 8.1

---
+++ 6.1/Program.cs
@@ -0,0 +1,128 @@
+﻿//Task 1
+
+//Console.WriteLine("Enter the number of workers");
+//var strnum = Console.ReadLine();
+//int workersAmount;
+//if (int.TryParse(strnum, out workersAmount))
+//{
+//    int total = 0;
+//    for (int i = 0; i < workersAmount; i++)
+//    {
+//        Console.WriteLine("Enter the salary of one of the employees");
+//        var strsalary = Console.ReadLine();
+//        int salary;
+//        if (int.TryParse(strsalary, out salary))
+//        {
+//            total += salary;
+//        }
+//        else
+//        {
+//            Console.WriteLine("An invalid enter");
+//        }
+//    }
+//    double result = total / workersAmount;
+//    Console.WriteLine($"Average salary of workers: {result}");
+//}
+//else
+//{
+//    Console.WriteLine("An invalid enter");
+//}
+
+//Task 2
+
+//Console.WriteLine("Enter the rows amount");
+//var strrowsnum = Console.ReadLine();
+//var rowsAmount = int.Parse(strrowsnum);
+
+//for (int i = 1; i <= rowsAmount; i++)
+//{
+//    for (int j = 0; j < i; j++)
+//        Console.Write("*");
+//    Console.WriteLine();
+//}
+
+//Task 3
+
+//using System;
+
+//Console.WriteLine("Enter the number");
+//var input = Console.ReadLine();
+//var number = int.Parse(input);
+
+
+//for (int i = 0; i <= number; i++)
+//{
+//    int j = 2;
+//    while(j <= Math.Sqrt(i))
+//    {
+//        if (i % j != 0)
+//        {
+//            Console.WriteLine(i);
+//        }
+//        j++;
+//    }
+//}
+
+
+//Task 4
+
+//Console.WriteLine("Enter the password");
+//var input = Console.ReadLine();
+//var password = int.Parse(input);
+
+
+
+//Task 5
+
+//Console.WriteLine("Enter the number");
+//var input = Console.ReadLine();
+//var number = int.Parse(input);
+//ulong j = 0;
+//ulong k = 1;
+//ulong l;
+//for (int i = 0; i < number; i++)
+//{
+//    l = j + k;
+//    Console.WriteLine(l);
+//    j = k;
+//    k = l;
+//}
+
+
+//Task 6
+
+//Console.WriteLine("Enter the daily number of working hours");
+//var input = Console.ReadLine();
+//var workTime = int.Parse(input);
+
+//Console.WriteLine("Enter the hourly rate");
+//var input2 = Console.ReadLine();
+//var hourlyRate = double.Parse(input2);
+
+//Console.WriteLine($"Your daily fee is {workTime * hourlyRate}");
+
+
+//Task 7
+
+//Console.WriteLine("Enter the number");
+//var input = Console.ReadLine();
+//var number = int.Parse(input);
+
+//Console.WriteLine($"multiplication table for {number}:");
+//for (int i = 1; i <= 10; i++)
+//{
+//    int number2 = number * i;
+//    Console.WriteLine(number2);
+//}
+
+
+//Task 8
+
+//Console.WriteLine("Enter the number");
+//var input = Console.ReadLine();
+//var number = int.Parse(input);
+
+//for (int i = 2; i < Math.Sqrt(number); i++)
+//{
+//    if ()
+//}
---
+++ 8.1/Program.cs
@@ -0,0 +1,97 @@
+﻿
+//int[] array = new int[10];
+//int sum = 0;
+//for (int i = 0; i < array.Length; i++)
+//{
+//    var random = new Random();
+//    array[i] = random.Next(-10, 10);
+//    if (i % 2 == 0)
+//    {
+//        Console.WriteLine(array[i]);
+//    }
+//    sum = sum + array[i]; // Якщо треба написати чи є невід'ємною сумма саме виведених чисел то треба просто перенести цю строку в if нагорі
+//}
+//if (sum >= 0)
+//{
+//    Console.WriteLine("The sum of the numbers is not negative");
+//}
+//else
+//{
+//    Console.WriteLine("The sum of the numbers is negative");
+//}
+
+
+
+
+//int[,] tableArray = new int[9, 9];
+//for (int i = 0; i < 9; i++)
+//{
+//    Console.WriteLine();
+//    for (int j = 0; j < 9; j++)
+//    {
+//        tableArray[i, j] = (i + 1) * (j + 1);
+//        Console.Write(tableArray[i, j] + " ");
+//    }
+//}
+
+
+
+//int greatestNum = 0;
+//int smallestNum = 0;
+//int[,] array = new int[5, 5];
+//for (int i = 0; i < 5; i++)
+//{
+//    Console.WriteLine();
+//    for (int j = 0; j < 5; j++)
+//    {
+//        var random = new Random();
+//        array[i, j] = random.Next(-10, 10);
+//        Console.Write($"{array[i, j]} ");
+//        if (greatestNum <= array[i, j])
+//        {
+//            greatestNum = array[i, j];
+//        }
+//        if (smallestNum >= array[i, j])
+//        {
+//            smallestNum = array[i, j];
+//        }
+//    }
+//}
+//Console.WriteLine();
+//Console.WriteLine($"The biggest number is {greatestNum}");
+//Console.WriteLine($"The smallest number is {smallestNum}");
+
+//for (int i = 0; i < 5; i++)
+//{
+//    for (int j = 0; j < 5; j++)
+//    {
+//        if (array[i, j] == greatestNum)
+//        {
+//            Console.WriteLine($"Index of the biggest number is {i}, {j}");
+//        }
+//        if (array[i, j] == smallestNum)
+//        {
+//            Console.WriteLine($"Index of the smallest number is {i}, {j}");
+//        }
+//    }
+//}
+
+
+
+//Console.WriteLine("Enter the number of days");
+//var input = Console.ReadLine();
+//var daysNum = int.Parse(input);
+
+//int day = daysNum % 7;
+//Console.WriteLine((Days)day);
+
+//enum Days
+//{
+//    Monday,
+//    Tuesday,
+//    Wednesday,
+//    Thursday,
+//    Friday,
+//    Saturday,
+//    Sunday
+//}
