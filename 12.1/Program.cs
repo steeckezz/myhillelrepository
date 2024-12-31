6de04a8645c605a3e21bd6bc0c0ab990f8e2fe46
Mykola <shyshkinmykola@gmail.com>
2024-12-31 21:33:18 +0100

    Completed task 12.1

---
+++ 12.1/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _12._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number");
+            var input = Console.ReadLine();
+            Console.WriteLine($"This number in Fibonacci sequence: {Fibonacci.FindByIndex(int.Parse(input) - 1)}");
+        }
+    }
+    public static class Fibonacci
+    {
+        public static int FindByIndex(int index)
+        {
+            if (index <= 1)
+            {
+                return index;
+            }
+            return FindByIndex(index - 1) + FindByIndex(index - 2);
+        }
+    }
+}
---
+++ 16.1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _16._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
